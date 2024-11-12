313ec776a749d8f6da7b79c283e310a6f08a78bd
jasanch <jasanch@bancolombia.com.co>
2024-11-12 01:20:06 -0500

    se crea cariables con las que se hara el crud y se crea el metodo listar(obtener)

--- src/main/java/org/example/controller/FuncionarioController.java
+++ src/main/java/org/example/controller/FuncionarioController.java
@@ -1,4 +1,7 @@
 package org.example.controller;
 
+import org.example.dao.FuncionarioDao;
+
 public class FuncionarioController {
+
 }
--- src/main/java/org/example/dao/FuncionarioDao.java
+++ src/main/java/org/example/dao/FuncionarioDao.java
@@ -1,4 +1,36 @@
 package org.example.dao;
 
+import org.example.domain.FuncionarioDomain;
+import org.example.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FuncionarioDao {
+
+    //CRUD JAVA Y SQL
+    private static final String GET_ALL_FUNCIONARIOS = "SELECT * FROM funcionario";
+    private static final String CREATE_FUNCIONARIO = "INSERT INTO funcionario (tipo_id, nombres, apellidos, estado_civil, sexo, direccion, telefono, fecha_nacimiento) VALUES ('?', '?', '?', '?', '?', '?', '?',  '?')";
+    private static final String GET_FUNCIONARIO_BY_ID = "SELECT * FROM funcionario WHERE id = ?";
+    private static final String UPDATE_FUNCIONARIO = "UPDATE funcionario SET tipo_id = ?, nombres = ?, apellidos = ?, estado_civil = ?, sexo = ?, direccion = ?, telefono = ?, fecha_nacimiento = ?";
+    private static final String DELETE_FUNCIONARIO = "DELETE FROM funcionario WHERE id = ?";
+
+    //Metodo para listar funcionarios, obtener funcionario
+    public List<FuncionarioDomain> obtenerFuncionarios() throws SQLException {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        List<FuncionarioDomain> funcionarios = new ArrayList<>();
+
+        try {
+            connection = ConnectionUtil.getConnection();
+            preparedStatement = connection.prepareStatement(GET_ALL_FUNCIONARIOS);
+            resultSet = preparedStatement.getResultSet();
+        }
+    }
 }
