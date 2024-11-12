6adac87cd3fd0577efa3bfb4f070484393430a69
jasanch <jasanch@bancolombia.com.co>
2024-11-12 00:54:02 -0500

    en domain se crea las propiedades y los getter and setter y el to string

--- src/main/java/org/example/domain/FuncionarioDomain.java
+++ src/main/java/org/example/domain/FuncionarioDomain.java
@@ -1,4 +1,100 @@
 package org.example.domain;
 
 public class FuncionarioDomain {
+    private String id;
+    private String tipo_id;
+    private String nombres;
+    private String apellidos;
+    private String estado_civil;
+    private String Sexo;
+    private String direccion;
+    private String telefono;
+    private String fecha_nacimiento;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTipo_id() {
+        return tipo_id;
+    }
+
+    public void setTipo_id(String tipo_id) {
+        this.tipo_id = tipo_id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEstado_civil() {
+        return estado_civil;
+    }
+
+    public void setEstado_civil(String estado_civil) {
+        this.estado_civil = estado_civil;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String sexo) {
+        Sexo = sexo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionarios{" +
+                "id='" + id + '\'' +
+                ", tipo_id='" + tipo_id + '\'' +
+                ", nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", estado_civil='" + estado_civil + '\'' +
+                ", Sexo='" + Sexo + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", fecha_nacimiento='" + fecha_nacimiento + '\'' +
+                '}';
+    }
 }
